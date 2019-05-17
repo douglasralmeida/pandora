251d72122702004c6cf7c31da481ea34f0e9b2d8
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-05-17 15:48:16 -0300

    Módulo de Modelagem de Processos

---
+++ Modelagem/Editor.cs
@@ -0,0 +1,76 @@
+﻿using Base;
+using System.Xml.Linq;
+
+namespace Modelagem
+{
+    class Editor
+    {
+        private string nomeArquivo;
+
+        private bool modificado;
+
+        private Pacote pacote;
+
+        public Editor()
+        {
+            nomeArquivo = "sem nome.pandorapac";
+            modificado = true;
+        }
+
+        public bool abrir(string nomeArquivo)
+        {
+            XElement xml;
+
+            xml = XElement.Load(nomeArquivo);
+            if (processarXML(xml))
+            {
+                modificado = false;
+                this.nomeArquivo = nomeArquivo;
+                return true;
+            }
+
+            return false;
+        }
+
+        private XElement gerarXML()
+        {
+            XElement xml;
+
+            xml = pacote.dataToXml();
+
+            return xml;
+        }
+
+        public bool getModificado()
+        {
+            return modificado;
+        }
+
+        public string getNomeArquivo()
+        {
+            return nomeArquivo;
+        }
+
+        private void novo(string usuarioGerador)
+        {
+            pacote = new Pacote(usuarioGerador);
+        }
+
+        private bool processarXML(XElement xml)
+        {
+            pacote = new Pacote("sem nome");
+
+            return pacote.xmlToData(xml);
+        }
+
+        public void salvar(string nomeArquivo)
+        {
+            XElement xml;
+
+            xml = gerarXML();
+            xml.Save(nomeArquivo);
+            modificado = false;
+            this.nomeArquivo = nomeArquivo;
+        }
+    }
+}
